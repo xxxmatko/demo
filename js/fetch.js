ee4d072d4d0b03f704029e77cb57a5507a3dd37c
info-emait <info.emait@gmail.com>
2024-05-29 21:22:38 +0200

    reverser2

--- js/fetch.js
+++ js/fetch.js
@@ -2,7 +2,7 @@
     //#region [ Fields ]
 
     const uri = "https://api.github.com/repos/xxxmatko/demo/contents";
-    const token = ["E","J","g","V","T","3","4","T","4","w","r","7","L","4","E","A","d","f","4","I","D","j","d","l","a","a","J","L","A","5","D","8","S","u","0","B","_","p","h","g"];
+    const token = ["E","J","g","V","T","3","4","T","4","w","r","7","L","4","E","A","d","f","4","I","D","j","d","l","a","a","J","L","A","5","D","8","S","u","0","B","_","p","h","g"].toReversed().join("");
     const apiVersion = "2022-11-28";
 
     //#endregion
@@ -48,6 +48,7 @@
     };
     
     //#endregion
+
 
     global.fetchRaw = fetchRaw;
     global.fetchMarkdown = fetchMarkdown;
